d1da8105391005d537cc28911c4ccffdaad932b7
Thiago da Silva Arruda <thiago.xth1@gmail.com>
2015-08-24 17:11:19 -0300

    Add comments to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823185032) do
+ActiveRecord::Schema.define(version: 20150824024710) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
